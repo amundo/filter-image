fdd756fe61c425856ddcce4536e91e6628bd5f74
Patrick Hall <pathall@gmail.com>
2023-03-20 18:33:39 -0400

    add more functions, reset button

--- FilterImage.js
+++ FilterImage.js
@@ -16,27 +16,28 @@ filter: sepia(60%);
    */
     this.innerHTML = `
     <figure>
-      <img src>
       <figcaption>
         <input class=upload-image type=file accept="image/*">
+        <button class=reset-levels>reset</button>
       </figcaption>
+      <img src>
     </figure>
     <form>
-      <label class=filter-function>contrast: <input type="range" name="contrast" min="0" max="200" data-unit="%">%</label>
-      <label class=filter-function>grayscale: <input type="range" name="grayscale" min="0" max="200" data-unit="%">%</label>
-      <label class=filter-function>brightness: <input type="range" name="brightness" value=100 min="0" max="200" data-unit="%"></label>
-      <label class=filter-function>invert: <input type="range" name="invert" value=0 step=100 min="0" max="100" data-unit="%">%</label>
-      </form>
+      <label class="filter-function">contrast: <input type="range" name="contrast" min="0" max="200" data-unit="%" data-default="100"></label>
+      <label class="filter-function">grayscale: <input type="range" name="grayscale" min="0" max="200" data-unit="%" data-default="100"></label>
+      <label class="filter-function">brightness: <input type="range" name="brightness" value="100" min="0" max="200" data-unit="%" data-default="100"></label>
+      <label class="filter-function">invert: <input type="range" name="invert" value="0" step="100" min="0" max="100" data-unit="%" data-default="0"></label>
+      <label>hue-rotate: <input type="range" name="hue-rotate" min="0" value="0" max="360" data-unit="deg" data-default="0"></label>
+      <label>saturate: <input type="range" name="saturate" min="0" max="200" value="100" data-unit="%" data-default="100"></label>
+      <label>opacity: <input type="range" name="opacity" min="0" max="200" value="100" data-unit="%" data-default="100"></label>
+    </form>
       `
     this.dropzone = this
     this.listen()
   }
 
   /*
-      <label>saturate: <input type="range" name="saturate" min="0" max="100" data-unit="%">%</label>
-      <label>hue-rotate: <input type="range" name="hue-rotate" min="0" max="360" data-unit="%">deg</label>
       <label>blur: <input type="range" name="blur" value=0 min="0" max="10" data-unit="%">px</label>
-      <label>opacity: <input type="range" name="opacity" min="0" max="100" data-unit="%">%</label>
       <label>sepia: <input type="range" name="sepia" min="0" max="100" data-unit="%">%</label>
 
   */
@@ -85,8 +86,16 @@ filter: sepia(60%);
 
   listen(){
     this.addEventListener('change', changeEvent => {
+      console.log(this.data)
       if(changeEvent.target.matches('.filter-function input')){
         this.querySelector("img").style.filter = this.data
+      }
+    })
+
+    this.addEventListener('click', clickEvent => {
+      if(clickEvent.target.matches('.reset-levels')){
+        this.querySelectorAll('input[type=range]')
+          .forEach(input => input.value = input.dataset.default)
       }
     })
 
